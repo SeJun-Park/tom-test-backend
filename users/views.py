7f25c2dd4fb7de26d67036aa409c7e6ee37e0a5d
SeJun-Park <sejun9aldo@gmail.com>
2023-05-31 02:28:03 +0900

    kakao redirect uri

--- users/views.py
+++ users/views.py
@@ -61,7 +61,8 @@ class IsPlayerKakaoLogIn(APIView):
             data={
                 "grant_type" : "authorization_code",
                 "client_id" : "d2703f278acadc861b3685bb7368adfb",
-                "redirect_uri" : "http://127.0.0.1:3001/kakaologin",
+                "redirect_uri" : "https://tom-test-frontend.onrender.com/kakaologin",
+                # "redirect_uri" : "http://127.0.0.1:3001/kakaologin",
                 "code" : code
             })
 
