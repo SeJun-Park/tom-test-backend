e9cb95cacd87eac469018df76af29cf972510b6a
SeJun-Park <sejun9aldo@gmail.com>
2023-09-14 22:37:06 +0900

    kakaologin redirect URI fix

--- users/views.py
+++ users/views.py
@@ -61,7 +61,7 @@ class IsPlayerKakaoLogIn(APIView):
             },
             data={
                 "grant_type" : "authorization_code",
-                "client_id" : "d2703f278acadc861b3685bb7368adfb",
+                "client_id" : "1c4cb9fa293fd2cb3a132f04abaae253",
                 "redirect_uri" : "https://3manofthematch.com/kakaologin",
                 # "redirect_uri" : "http://127.0.0.1:3001/kakaologin",
                 "code" : code
